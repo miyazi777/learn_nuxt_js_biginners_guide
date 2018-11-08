3cde66d2fd9d4cb8bbf2291cea77c00338c22266
miyazi777 <m71203@gmail.com>
2018-11-08 22:50:23 +0900

    add section19

---
+++ chapter3/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+   <span>default layout</span>
+   <AppNavigation/>
+
+   <hr>
+
+   <nuxt/>
+
+   <footer>
+     footer
+   </footer>
+
+  </div>
+</template>
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+
+export default {
+components: {
+    AppNavigation
+  }
+}
+</script>
+
+<style>
+</style>
