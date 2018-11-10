394d6b4cfe86df798ffa2c377468beed4bb3def6
miyazi777 <m71203@gmail.com>
2018-11-10 11:03:30 +0900

    add section22

---
+++ chapter3-s22/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <nuxt-link to='page1'>page1</nuxt-link>
+    <nuxt-link to='page2'>page2</nuxt-link>
+  </div>
+</template>
+
---
+++ chapter3-s22/pages/page2.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>page2</h2>
+    <nuxt-link to='/'>top</nuxt-link>
+  </div>
+</template>
